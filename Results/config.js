0723d0de7b474cd0774d9b68eba61d5e2b5d9d6e
IZZATHUL-ISLAM-MADRASA <izzathulislamwebsite@gmail.com>
2025-12-24 12:37:22 +0530

    Create config.js

---
+++ Results/config.js
@@ -0,0 +1,16 @@
+ // API Configuration
+        const API_OFFLINE = "https://script.google.com/macros/s/AKfycbz2TPisz6yHeSJ6koNafGmAxvzKAiXvBzVsu8LeDvu0UnXWseMMr6DoNIVjF0n7rZc0Cg/exec";//"https://script.google.com/macros/s/AKfycbxgYi-QeCjL72FSM_jWExzgeFGIp44_ciOY-GwjGkuHaisi6ZvWVyw3NVEKeiv511nRgQ/exec";
+        const API_ONLINE = "https://script.google.com/macros/s/AKfycbwnu4GBO6d6qzkIPF4E-y5zKcpvCVn4Ke561dGc4U3QwxZQvc_QnO7K47-t6UCkT3QnXQ/exec";//"https://script.google.com/macros/s/AKfycbwXGHxq87nwa-vVAEObZgQpOqWLTM9JPBidk8aOX9N90VDoJ1q0TubVLUTpS0f1An0T/exec";
+        
+        const sheetid_online = {
+            "Term-I": "13Sxl044nBWFc7ViMjSvQACZ-CmHUg-r2sjCF80fzV08",
+            "Term-II": "1Gsx_IaSWsbOazJl6STayu9rjxhrdrkYVdLRTpDgVDgY",
+            "Annual-Term": "Annual_Term_2023_24"
+        };
+
+        const sheetid_offline = {
+            "Term-I": "1UQWMipKWOPhA8OOPfalTVpS-wy6Iwzd4qz5gBD8ly20",
+            "Term-II": "1QMUhrH3hR_yHv2-Kt3IPVhRUn2mUI7J6UKsEWo5s-LA",
+            "Annual-Term": "Annual_Term_2023_24"
+        };
+        
